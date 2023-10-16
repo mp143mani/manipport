9875b67db4aa53b9f87621ac83faed831d61c9b1
mp143mani <127532509+mp143mani@users.noreply.github.com>
2023-10-16 19:07:54 +0530

    s

--- src/Components/Home/Profile.js
+++ src/Components/Home/Profile.js
@@ -1,34 +1,41 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import {useTypewriter,Cursor} from 'react-simple-typewriter'
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 // import Typical from "react-typical";
 import "./Profile.css";
 
 export default function Profile() {
-  let navigate = useNavigate()
+  let navigate = useNavigate();
   const [typei] = useTypewriter({
-    words:['Full stack Developer'],
-    loop:{},
-    typeSpeed:150,
-    delaySpeed:50
-  })
+    words: ["Full stack Developer"],
+    loop: {},
+    typeSpeed: 150,
+    delaySpeed: 50,
+  });
   return (
-    <div className="profile-container" id="home">
-      <div className="profile-parent">
+    <div className="maincontainer" id="home">
+      <div className="profile2">
         <div className="profile-details">
           <div className="colz">
             <div className="colz-icon">
-                
-              <a  target="_blank" onClick={()=>navigate('https://github.com/mp143mani')}>
-              <i className="fab fa-github fa-3x" style={{ color: '#c2c2c2' }}></i>
-
-
-             
-
+              <a
+                target="_blank"
+                onClick={() => navigate("https://github.com/mp143mani")}
+              >
+                <i
+                  className="fab fa-github fa-2x"
+                  style={{ color: "#c2c2c2" }}
+                ></i>
               </a>
-              <a href="https://www.linkedin.com/in/manivenktraj-m-560b20287/" target="_blank"><i className="fa-brands fa-linkedin" style={{color: '#0743ab'}} /></a>
-             
-            
+              <a
+                href="https://www.linkedin.com/in/manivenktraj-m-560b20287/"
+                target="_blank"
+              >
+                <i
+                  className="fa-brands fa-linkedin fa-2x"
+                  style={{ color: "#0A66C2" }}
+                />
+              </a>
             </div>
           </div>
 
@@ -39,17 +46,25 @@ export default function Profile() {
             </span>
           </div>
           <div className="profile-details-role">
-             <span className="primary-text">
-             <h1>
-              I'm a 
-              <span style={{fontWeight:"bold",color:"green",paddingLeft:"25px"}}>{typei}</span>
-             </h1>
+            <span className="primary-text">
+              <h1>
+                I'm a
+                <span
+                  style={{
+                    fontWeight: "bold",
+                    color: "green",
+                    paddingLeft: "25px",
+                  }}
+                >
+                  {typei}
+                </span>
+              </h1>
 
-              <span className="profile-role-tagline">
+              <span className="profile-role-info">
                 I have always had a great desire, becoming more of a person
                 where I can find challenges in the work environment
               </span>
-            </span> 
+            </span>
           </div>
           <div className="profile-options">
             <button className="btn primary-btn"> Hire Me </button>
